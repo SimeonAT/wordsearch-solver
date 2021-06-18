aaeb145cf770cc7999010f4e340f037e8d83441e
Simeon <siatran@ucsc.edu>
2021-06-18 14:31:30 -0700

    Allowed for command line arguments to be entered for word search image and tesseract traning data

--- read.py
+++ read.py
@@ -8,26 +8,41 @@ def print_word_search(word_search):
         search matrix in a row major format. """
     for line in word_search:
         print(line)
+    return
 
-pytesseract.pytesseract.tesseract_cmd = "tesseract"
+def read_word_search(file_loc, training_data):
+    """ This function utilizes Tesseract to read in a word search and
+        stores it as a 2D array.
 
-# You need the ABSOLUTE PATH of tessdata for the "tessdata-dir" parameter
-config = r"--tessdata-dir /home/simeon/wordsearch-solver/tessdata_old"
+        Parameter(s):
+            the file location of the image.
+            the Tesseract training data folder to utilize.
 
-try:
-    file_loc = sys.argv[1]
-    word_search = cv2.imread(file_loc)
-except:
-    print("Please enter only 1 valid file location.\n")
+        Returns:
+            a 2D matrix representation of the word search.
+            will return 'False' upon error. """
+    pytesseract.pytesseract.tesseract_cmd = "tesseract"
 
-# PyTesseract uses RGB, OpenCV uses BGR. Change BGR image read by OpenCV
-# into an RGB image for Tesseract. 
-word_search = cv2.cvtColor(word_search, cv2.COLOR_BGR2RGB)
+    try:
+        # You need the ABSOLUTE PATH of tessdata for the "tessdata-dir" parameter
+        config = r"--tessdata-dir /home/simeon/wordsearch-solver/" + training_data
+        word_search = cv2.imread(file_loc)
+    except:
+        print("Please enter only 1 valid file location.\n")
+        print("Command Line Argument Format:")
+        print("\tpython3 read.py [file location] [tesseract training data]\n")
+        return False
 
-# The word search will be represented as a 2D array
-word_search_matrix = pytesseract.image_to_string(word_search, config=config).split()
-for i in range(0, len(word_search_matrix)):
-    word_search_matrix[i] = list(word_search_matrix[i])
+    # PyTesseract uses RGB, OpenCV uses BGR. Change BGR image read by OpenCV
+    # into an RGB image for Tesseract. 
+    word_search = cv2.cvtColor(word_search, cv2.COLOR_BGR2RGB)
 
-print_word_search(word_search_matrix)
-cv2.destroyAllWindows()
+    # The word search will be represented as a 2D array
+    word_search_matrix = pytesseract.image_to_string(word_search, config=config).split()
+    for i in range(0, len(word_search_matrix)):
+        word_search_matrix[i] = list(word_search_matrix[i])
+
+    return word_search_matrix
+
+if __name__ == "__main__":
+    print_word_search(read_word_search(sys.argv[1], sys.argv[2]))
