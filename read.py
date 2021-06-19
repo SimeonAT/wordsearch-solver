a60e4c50718ced52f0307e1cc1b8889a13052386
Simeon <siatran@ucsc.edu>
2021-06-19 11:11:14 -0700

    Formatted code

--- read.py
+++ read.py
@@ -19,8 +19,14 @@ def bounding_boxes(image, config):
     print(boxes)
     for box in boxes.splitlines():
         char, x, y, width, height, _ = box.split(" ")
+
+        # The bounding box coordinates are in a different format that I couldn't
+        # fully figure out. As a result, I used the code for the top left and bottom
+        # coordinates in the video https://www.youtube.com/watch?v=6DjFscX4I_c to build
+        # the bounding boxes.
         top_left = (int(x), int(img_height) - int(y))
         bottom_right = (int(width), int (img_height) - int(height))
+
         cv2.rectangle(image, top_left, bottom_right, (0, 0, 255), 2)
         cv2.circle(image, top_left, 3, (0, 0, 255), -1)
         cv2.circle(image, bottom_right, 3, (0, 255, 0), -1)
@@ -28,6 +34,7 @@ def bounding_boxes(image, config):
     cv2.imshow("Bounding Boxes Debug", image)
     cv2.waitKey(0)
     return
+
 
 def print_word_search(word_search):
     """ Debugging function that prints the contents of the word
@@ -47,20 +54,20 @@ def read_word_search(file_loc, training_data):
         Returns:
             a 2D matrix representation of the word search.
             will return 'False' upon error. """
+
     pytesseract.pytesseract.tesseract_cmd = "tesseract"
 
     try:
         # You need the ABSOLUTE PATH of tessdata for the "tessdata-dir" parameter
         config = r"--tessdata-dir /home/simeon/wordsearch-solver/" + training_data \
             + " --oem 3 --psm 6 load_system_dawg=false load_freq_dawg=false"
+
         word_search = cv2.imread(file_loc)
     except:
         print("Please enter only 1 valid file location.\n")
         print("Command Line Argument Format:")
         print("\tpython3 read.py [file location] [tesseract training data]\n")
         return False
-
-    bounding_boxes(word_search, config)
 
     # PyTesseract uses RGB, OpenCV uses BGR. Change BGR image read by OpenCV
     # into an RGB image for Tesseract. 
@@ -71,6 +78,7 @@ def read_word_search(file_loc, training_data):
         word_search_matrix[i] = list(word_search_matrix[i])
 
     return word_search_matrix
+
 
 if __name__ == "__main__":
     word_search = read_word_search(sys.argv[1], sys.argv[2])
