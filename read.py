f95382e00fd823f0fc83b87a40ee66a425441f36
Simeon <siatran@ucsc.edu>
2021-06-22 00:55:34 -0700

    Cited sources, added another debugging function.

--- read.py
+++ read.py
@@ -2,6 +2,19 @@ import cv2
 import pytesseract
 import sys
 
+def display_image(image):
+    """ Debugging function that uses OpenCV to display
+        a given image until any key has been pressed.
+
+        I learned that cv2.waitKey() function closed when a key has been
+        pressed from the following link:
+        https://docs.opencv.org/3.4/d7/dfc/group__highgui.html#ga5628525ad33f52eab17feebcfba38bd7
+    """
+    cv2.imshow("Debugging Image", image)
+    cv2.waitKey(0)
+    return
+
+
 def bounding_boxes(image, config):
     """ Debugging function that displays the bounding boxes
         of each detected character in the given OpenCV image.
@@ -24,6 +37,7 @@ def bounding_boxes(image, config):
         # fully figure out. As a result, I used the code for the top left and bottom
         # coordinates in the video https://www.youtube.com/watch?v=6DjFscX4I_c to build
         # the bounding boxes.
+        # 
         top_left = (int(x), int(img_height) - int(y))
         bottom_right = (int(width), int (img_height) - int(height))
 
@@ -43,6 +57,7 @@ def print_word_search(word_search):
         print(line)
     return
 
+
 def read_word_search(file_loc, training_data):
     """ This function utilizes Tesseract to read in a word search and
         stores it as a 2D array.
@@ -57,29 +72,26 @@ def read_word_search(file_loc, training_data):
 
     pytesseract.pytesseract.tesseract_cmd = "tesseract"
 
-    try:
-        # You need the ABSOLUTE PATH of tessdata for the "tessdata-dir" parameter
-        config = r"--tessdata-dir /home/simeon/wordsearch-solver/" + training_data \
+    # You need the ABSOLUTE PATH of tessdata for the "tessdata-dir" parameter
+    config = r"--tessdata-dir /home/simeon/wordsearch-solver/" + training_data \
             + " --oem 3 --psm 6 load_system_dawg=false load_freq_dawg=false"
 
-        word_search = cv2.imread(file_loc)
-    except:
-        print("Please enter only 1 valid file location.\n")
-        print("Command Line Argument Format:")
-        print("\tpython3 read.py [file location] [tesseract training data]\n")
-        return False
+    word_search = cv2.imread(file_loc)
 
     # PyTesseract uses RGB, OpenCV uses BGR. Change BGR image read by OpenCV
-    # into an RGB image for Tesseract. 
+    # into an RGB image for Tesseract. I learned about this insight from 
+    # https://www.youtube.com/watch?v=6DjFscX4I_c.i
+    # 
     word_search = cv2.cvtColor(word_search, cv2.COLOR_BGR2RGB)
 
     word_search_matrix = pytesseract.image_to_string(word_search, config=config).split()
     for i in range(0, len(word_search_matrix)):
         word_search_matrix[i] = list(word_search_matrix[i])
 
+    bounding_boxes(word_search, config)
     return word_search_matrix
 
 
 if __name__ == "__main__":
     word_search = read_word_search(sys.argv[1], sys.argv[2])
-    # print_word_search(word_search)
+    print_word_search(word_search)
