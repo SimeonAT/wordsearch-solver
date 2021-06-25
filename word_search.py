a96afe16121f0a6a70ebf474e060e25e74845d99
Simeon <siatran@ucsc.edu>
2021-06-24 22:58:02 -0700

    Wrote docstring for evaluate() function and added 'enums' for north, south, east, west directions.

--- word_search.py
+++ word_search.py
@@ -1,3 +1,28 @@
+NORTH = 1
+SOUTH = 2
+EAST = 3
+WEST = 4
+
+def evaluate(word_search, word, direction, row, column):
+    """ Helper function for find_words() that looks to see if
+        the word in find is present starting at (row, column) and continuing
+        in the direction North, South, East, or West.
+
+        Parameters:
+            the word search as a matrix
+            the word to find
+            the direction to look in
+              - NORTH = 1
+              - SOUTH = 2
+              - EAST = 3
+              - WEST = 4
+            the row and column of the letter of the word
+
+        Returns:
+            True if the word is at (row, column) and in the given direction,
+            and False otherwise. """
+    raise NotImplementedError
+
 
 def find_words(word_search, list_of_words):
     """ This function contains the actual implementation of the
